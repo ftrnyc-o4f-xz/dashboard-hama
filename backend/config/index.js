da5e2346d055b32acf31a47ac26627cd1a99c86e
fitrinyc <novyanti.fitria04@gmail.com>
2026-02-10 13:33:23 +0700

    fix: set mongo env and debug auth

--- backend/config/index.js
+++ backend/config/index.js
@@ -3,6 +3,6 @@ dotenv.config();
 
 export default {
   apiPrefix: "/v1",
-  dbUrl: process.env.MONGODB_URI,
+  dbUrl: process.env.MONGODB_URL,
   jwtSecret: process.env.JWT_SECRET || "smart_farm_secret_key",
 };
